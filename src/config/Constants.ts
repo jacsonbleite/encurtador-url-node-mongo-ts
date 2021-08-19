8beeecd14b3f1dc897d4ed1ed08eeb1b669c76b7
Jacson B. Leite <42217315+jacsonbleite@users.noreply.github.com>
2021-08-19 17:08:09 -0400

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,6 +1,6 @@
 export const config = {
     URL_API: 'http://localhost:5000',
-    MONGO_CONNECTION: 'mongodb+srv://JackDB:aKthOVOW8yRHdpYE@url-shortener-dio.x8une.mongodb.net/url-shortener-dio?retryWrites=true&w=majority'
+    MONGO_CONNECTION: 'mongodb+srv://<USER DB>:<PASS DB>@url-shortener-dio.x8une.mongodb.net/<DB>?retryWrites=true&w=majority'
 }
 
 
